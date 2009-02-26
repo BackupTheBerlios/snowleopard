cf6cd7b98d2bba435667e132fd57a1ccceaf9824
rsergeant <rsergeant>
2009-02-26 12:42:04 +0000

    More game institute samples added

---
+++ trunk/src/test/other/game_institute/cpp1_prg_0212.cpp
@@ -0,0 +1,55 @@
+
+/*-
+ * Copyright (c) 2009 Roel Sergeant.
+ * All rights reserved.
+ *
+ * This code is derived from software contributed to the Snow Leopard 
+ * project.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE AUTHOR AND CONTRIBUTORS ``AS IS'' AND ANY 
+ * EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED 
+ * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE 
+ * DISCLAIMED.  IN NO EVENT SHALL THE FOUNDATION OR CONTRIBUTORS BE LIABLE FOR 
+ * ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL 
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR 
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER 
+ * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, 
+ * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+/* Standard C++ headers */
+#include <iostream>
+
+/*
+ * This program demonstrates a for loop with unsequential variables.
+ */
+int
+main ()
+{
+	using std::cout;
+	using std::endl;
+
+	int sum = 0;
+	for (int i = 1; i <= 100; ++i)
+		sum += i;
+
+	cout << "The sum of 1 - 100 is " << sum << endl;
+
+	sum = 0;
+	for (int i = 1; i <= 100; i += 2)
+		sum +=i;
+
+	cout << "The sum of odd number < 100  is " <<  sum << endl;
+}
+
+/*>- EOF -<*/
