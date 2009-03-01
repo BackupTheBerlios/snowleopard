aea3a244083302b8dc2d336e9feee215006b4414
rsergeant <rsergeant>
2009-03-01 19:21:19 +0000

    Lesson 1 exercises

---
+++ trunk/src/test/other/game_institute/cpp1_prg_0102.cpp
@@ -0,0 +1,63 @@
+
+/*-
+ * Copyright (c) 2009 Roel Sergeant.
+ * All rights reserved.
+ *
+ * This code is derived from software contributed to the Snow Leopard 
+ * project.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE AUTHOR AND CONTRIBUTORS ``AS IS'' AND ANY 
+ * EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED 
+ * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE 
+ * DISCLAIMED.  IN NO EVENT SHALL THE FOUNDATION OR CONTRIBUTORS BE LIABLE FOR 
+ * ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL 
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR 
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER 
+ * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, 
+ * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE 
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+/* Standard C++ headers */
+#include <iostream>
+
+/*
+ * This program reads values of different types entered by the user.
+ */
+int
+main ()
+{
+	using std::cin;
+	using std::cout;
+	using std::endl;
+
+	char letter = 'A';
+	int integer = 0;
+	float dec = 0.0f;
+
+	cout << "Enter a letter: ";
+	cin >> letter;
+
+	cout << "Enter an integer: ";
+	cin >> integer;
+
+	cout << "Enter a float: ";
+	cin >> dec;
+	
+	cout << endl;
+	cout << "Letter: " << letter << endl;
+	cout << "Integer: " << integer << endl;
+	cout << "Float: " << dec << endl;
+	
+}
+
+/*>- EOF -<*/
