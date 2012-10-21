b55f770c382c3c3f44fe9b607b1f44246be851e9
rsergeant <rsergeant>
2012-10-21 09:18:50 +0000

    slcc: added more test suite files

---
+++ trunk/src/test/slcc/unit_test/makefiles/object_rules.mk
@@ -0,0 +1,39 @@
+#===============================================================================
+# Snow Leopard Test Suite
+#
+# Copyright (C) 2012 Roel Sergeant
+#
+# This program is free software: you can redistribute it and/or modify it under
+# the terms of the GNU General Public License as published by the Free
+# Software Foundation, either version 3 of the License, or (at your option) any
+# later version
+#
+# This program is distributed in the hope that it will be useful, but WITHOUT
+# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+# FOR A PARTICULAR PURPOSE. See the GNU General Public License for more 
+# details.
+#
+# You should have received a copy of the GNU General Public License along with
+# this program. If not, see <http://www.gnu.org/licenses/>
+#===============================================================================
+
+#-------------------------------------------------------------------------------
+# object_rules.mk
+#-------------------------------------------------------------------------------
+# Generated file containing the object file rules used for the compiler front 
+# end.
+#-------------------------------------------------------------------------------
+
+#-------------------------------------------------------------------------------
+# Rules
+#
+$(UT_RUN_1_PROGRAM)					: 
+
+##$(TST_UT_PATH)/run_1_private_linked_list_1.o		: 		\
+##		unit_test/run_1/run_1_private_linked_list_1.c
+##	$(CC) -c $(CFLAGS) 						\
+##		unit_test/run_1/run_1_private_linked_list_1.c		\
+##		-o $(.TARGET)
+
+#-------------------------------------------------------------------------------
+#<EOF>
---
+++ trunk/src/test/slcc/unit_test/makefiles/objects.mk
@@ -0,0 +1,37 @@
+#===============================================================================
+# Snow Leopard Test Suite
+#
+# Copyright (C) 2012 Roel Sergeant
+#
+# This program is free software: you can redistribute it and/or modify it under
+# the terms of the GNU General Public License as published by the Free
+# Software Foundation, either version 3 of the License, or (at your option) any
+# later version
+#
+# This program is distributed in the hope that it will be useful, but WITHOUT
+# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+# FOR A PARTICULAR PURPOSE. See the GNU General Public License for more 
+# details.
+#
+# You should have received a copy of the GNU General Public License along with
+# this program. If not, see <http://www.gnu.org/licenses/>
+#===============================================================================
+
+#-------------------------------------------------------------------------------
+# objects.mk
+#-------------------------------------------------------------------------------
+# Generated file containing the object files used for the compiler front end 
+# test suite.
+#-------------------------------------------------------------------------------
+
+##UT_RUN_1_PROGRAM	= $(TST_UT_PATH)/run_1_private_linked_list
+##UT_RUN_1_OBJS 		= \
+##	$(TST_UT_PATH)/run_1_private_linked_list_1.o		\
+##	$(TST_UT_PATH)/run_1_private_linked_list_1_main.o
+
+UT_PROGRAMS		= 
+
+UT_OBJS 		= 
+
+#-------------------------------------------------------------------------------
+#<EOF>
