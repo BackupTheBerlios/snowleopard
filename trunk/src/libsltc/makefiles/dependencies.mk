d214475890b501270667c532f20660d80d49f286
rsergeant <rsergeant>
2012-10-27 09:38:26 +0000

    libsltc: split dependencies and includes

---
+++ trunk/src/libsltc/makefiles/dependencies.mk
@@ -0,0 +1,92 @@
+#===============================================================================
+# Snow Leopard Typed Component Library
+#
+# Copyright (C) 2011, 2012 Roel Sergeant
+#
+# This program is free software: you can redistribute it and/or modify it under
+# the terms of the GNU General Public License as published by the Free
+# Software Foundation, either version 3 of the License, or (at your option) any
+# later version
+#
+# This program is distributed in the hope that it will be useful, but WITHOUT
+# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+# FOR A PARTICULAR PURPOSE. See the GNU General Public License for more 
+# details.
+#
+# You should have received a copy of the GNU General Public License along with
+# this program. If not, see <http://www.gnu.org/licenses/>
+#===============================================================================
+
+#-------------------------------------------------------------------------------
+# dependencies.mk
+#-------------------------------------------------------------------------------
+# Generated file containing a list of all dependencies that are specific to the
+# typed component library.
+#-------------------------------------------------------------------------------
+
+#-------------------------------------------------------------------------------
+# Header file dependencies
+#
+ARRAY_C_H_DEPS			= \
+	$(TC_INCLUDE)/config_tc.h
+
+ARRAY_H_DEPS			= \
+	$(TC_INCLUDE)/config_tc.h
+
+CHAR_FUNCTIONS_H_DEPS   	=
+
+CONFIG_TC_H_DEPS		= \
+	$(CONFIG_C_INCLUDES)
+
+DATE_FUNCTIONS_H_DEPS		=
+
+FILE_FUNCTIONS_H_DEPS		= 
+
+INTEGER_FUNCTIONS_H_DEPS	=
+
+LINKED_LIST_C_H_DEPS		= \
+	$(TC_INCLUDE)/config_tc.h
+
+LINKED_LIST_H_DEPS		= \
+	$(TC_INCLUDE)/config_tc.h
+
+TREE_C_H_DEPS			= \
+	$(TC_INCLUDE)/config_tc.h
+
+STRING_FUNCTIONS_H_DEPS		= \
+	$(TC_INCLUDE)/string_functions.h
+
+TREE_H_DEPS			= \
+	$(TC_INCLUDE)/config_tc.h
+
+VERSION_TC_H_DEPS		= \
+	$(CONFIG_C_INCLUDES)						\
+	$(VERSION_C_INCLUDES)
+
+#-------------------------------------------------------------------------------
+# Source file dependencies
+#
+CHAR_FUNCTIONS_DEPS		= \
+	$(TC_INCLUDE)/char_functions.h $(CHAR_FUNCTIONS_H_DEPS)
+
+DATE_FUNCTIONS_DEPS		= \
+	$(TC_INCLUDE)/date_functions.h $(DATE_FUNCTIONS_H_DEPS)
+
+FILE_FUNCTIONS_DEPS		= \
+	$(TC_INCLUDE)/file_functions.h $(FILE_FUNCTIONS_H_DEPS)		\
+	$(TC_INCLUDE)/string_functions.h $(STRING_FUNCTIONS_H_DEPS)
+
+INTEGER_FUNCTIONS_DEPS		= \
+	$(TC_INCLUDE)/integer_functions.h $(INTEGER_FUNCTIONS_H_DEPS)
+
+STRING_FUNCTIONS_DEPS	= \
+	$(TC_INCLUDE)/string_functions.h $(STRING_FUNCTIONS_H_DEPS)
+
+VERSION_TC_DEPS		= \
+	$(TC_INCLUDE)/config_tc.h $(CONFIG_TC_H_DEPS)			\
+	$(TC_INCLUDE)/version_tc.h $(VERSION_TC_H_DEPS)			\
+	$(CONFIG_C_INCLUDES)						\
+	$(VERSION_C_INCLUDES)
+
+#-------------------------------------------------------------------------------
+#<EOF>
